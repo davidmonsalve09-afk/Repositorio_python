f1bfb7576d0c78b01a3b4d343f4d8e4db6acdfe3
davidmonsalve09-afk <davidandolini09@gmail.com>
2025-10-03 16:57:24 -0500

    Creacion del Repositorio
    Codigo basico para suma em python

---
+++ suma.py
@@ -0,0 +1,10 @@
+def fun(x, y):
+    z = x + y
+    return z
+
+
+x = int(input("ingrese el primer numero: "))
+y = int(input("ingrese el segundo numero: "))
+
+
+print("la suma es igual a:", fun(x,y))
